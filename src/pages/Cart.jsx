5e5822f4eb4ede0cd62590cf86a4a199754d27f8
Aniket3689 <aniket.bcastudent21.6607@cimage.in>
2024-03-31 10:58:23 +0530

    styleadd

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -16,28 +16,29 @@ const Cart = () => {
         cart.length> 0 ? 
         (
     
-        <div className=" flex justify-between items-center">
-          <div className=" grid  xs:gridcols-1  max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
+        <div className=" flex ">
+          <div className=" grid  xs:gridcols-1   p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
               {cart.map((item, index) => {
                 return <CartItem key={index.id} item={item} itemIndex={index} />;
               })}
         </div>
-        <div  className="" >
-          <div>
+        <div  className="justify-between" >
+          <div  >
                   <div className="text-slate-500"> Your cart</div>
                   <div>Summary</div>
                   <p>
                     <span >Total Item {cart.length}</span>
                   </p>
                 </div>
+
+
                 <div >
-                  <p className=" outline  rounded-full items-center p-3
-                     hover:bg-slate-700 font-bold transation duration-300 ease-in
-                     hover:text-white
-                  ">
+                  <p>
                     Total amount :${totalAmount}
                   </p>
-             
+                 <button>
+                  checkout Now
+                 </button>
                 </div>
 
           </div>
